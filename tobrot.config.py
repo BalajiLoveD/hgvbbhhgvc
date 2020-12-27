4d8cad578dcaa18a2e842da292966c0e482e0735
BalajiLoveD <62749324+BalajiLoveD@users.noreply.github.com>
2020-12-27 15:50:07 +0530

    Update and rename tobrot/g_config.py to tobrot.config.py

--- tobrot.config.py
+++ tobrot.config.py
@@ -2,11 +2,11 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
+    TG_BOT_TOKEN= "1456031638:AAEHjOeROrmCeF0pKK0gt6XHPnNNmtLFePU"
+    APP_ID = 1366324
+    API_HASH = "0d0274833b354d9223ca60b3e2f1e7c8"
+    OWNER_ID = 708155068
+    AUTH_CHANNEL = [-1001329496388]
     DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
     RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
